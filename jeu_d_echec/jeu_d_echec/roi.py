50ec66c7850947a14f25ad08bbd824a61e88249c
tabegon <66781353+tabegon@users.noreply.github.com>
2025-04-01 16:50:25 +0200

    Update roi.py

--- jeu_d_echec/jeu_d_echec/roi.py
+++ jeu_d_echec/jeu_d_echec/roi.py
@@ -1,5 +1,21 @@
 def identification(i, j):
     print('La pièce est un roi')
 
-def move_piece(a,b,c,d,e,f):
-    print("fonction à coder")
+def move_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
+    i_variation = i_origine - i_clic
+    j_variation = j_origine - j_clic
+    if abs(i_variation) == 1 or abs(j_variation) == 1:
+        return True
+        
+
+def eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
+    if board[j_origine][i_origine] == "K":
+        piece = "w"
+    else:
+        piece = "b"
+    
+    piece_black = board[j_clic][i_clic]
+    if board[j_clic][i_clic] != ' ': 
+        if board[j_clic][i_clic] == piece_black.lower():
+            return False
+    return True
