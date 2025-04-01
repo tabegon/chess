05f2a783470d63721805077c4c463743e2e55d8d
tabegon <66781353+tabegon@users.noreply.github.com>
2025-04-01 17:24:05 +0200

    Update roi.py
    add found piece color and fix move piece

--- jeu_d_echec/jeu_d_echec/roi.py
+++ jeu_d_echec/jeu_d_echec/roi.py
@@ -1,21 +1,28 @@
 def identification(i, j):
     print('La pièce est un roi')
 
-def move_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
-    i_variation = i_origine - i_clic
-    j_variation = j_origine - j_clic
-    if abs(i_variation) == 1 or abs(j_variation) == 1:
-        return True
-        
-
-def eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
+def found_piece_color(board):
     if board[j_origine][i_origine] == "K":
         piece = "w"
     else:
         piece = "b"
-    
-    piece_black = board[j_clic][i_clic]
+    return piece
+
+def move_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
+    i_variation = i_origine - i_clic
+    j_variation = j_origine - j_clic
+    if abs(i_variation) <= 1 and abs(j_variation) <= 1:
+        return eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic)
+        
+
+def eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
+    piece = found_piece_color(board)
+    piece_clic = board[j_clic][i_clic]
     if board[j_clic][i_clic] != ' ': 
-        if board[j_clic][i_clic] == piece_black.lower():
-            return False
+        if piece == "b":
+            if board[j_clic][i_clic] == piece_clic.lower():
+                return False
+        elif piece == "w":
+            if board[j_clic][i_clic] == piece_clic.upper():
+                return False
     return True
