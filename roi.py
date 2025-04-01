545f42875f1294ad702a72a3a8360871ad218ecd
tabegon <theo.abegon@mail.com>
2025-04-01 20:34:16 +0200

    Intermédiaire, test roi

--- roi.py
+++ roi.py
@@ -7,7 +7,7 @@ import dame as Q
 def identification(i, j):
     print('La pièce est un roi')
 
-def found_piece_color(board):
+def found_piece_color(board, i_origine, j_origine):
     if board[j_origine][i_origine] == "K":
         piece = "w"
     else:
@@ -18,13 +18,13 @@ def move_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
     i_variation = i_origine - i_clic
     j_variation = j_origine - j_clic
     if abs(i_variation) <= 1 and abs(j_variation) <= 1:
-        mvmt_possible = eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic)
-    if mvmt_possible and echec_sur_arrive():
+        mvmt_possible = eat_piece(board,i_origine,j_origine,i_clic,j_clic)
+    if mvmt_possible and echec_sur_arrive(board,player_turn, i_origine,j_origine, i_clic,j_clic):
         return True
         
 
-def eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
-    piece = found_piece_color(board)
+def eat_piece(board,i_origine,j_origine,i_clic,j_clic):
+    piece = found_piece_color(board, i_origine, j_origine)
     piece_clic = board[j_clic][i_clic]
     if board[j_clic][i_clic] != ' ': 
         if piece == "b":
@@ -35,26 +35,30 @@ def eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
                 return False
     return True
 
-def echec_sur_arrive(board,player_turn,i_clic,j_clic):
+def echec_sur_arrive(board,player_turn, i_origine,j_origine, i_clic,j_clic):
     nb_ligne = -1
     nb_colonne = -1
-    piece = found_piece_color(board)
+    piece = found_piece_color(board, i_origine,j_origine)
     if piece == "b":
         chaine = "RBNQKP"
     elif piece == "w":
         chaine = "rbnqkp"
-    for i in chaine.split():
+    for i in chaine:
+        print(i)
         for ligne in board:
             nb_ligne += 1
             nb_colonne = -1
-            for case in ligne.split():
+            for case in ligne:
                 nb_colonne += 1
                 if case == i:
                     if piece == "b":
                         if i.move_piece(board, player_turn, nb_ligne, nb_colonne, i_clic, j_clic):
+                            print('False')
                             return False
                     elif piece == "w":
                         z = i.upper()
                         if z.move_piece(board, player_turn, nb_ligne, nb_colonne, i_clic, j_clic):
+                            print('False')
                             return False
+    print('True')
     return True
