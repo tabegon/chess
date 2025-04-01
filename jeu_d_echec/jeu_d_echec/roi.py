2b154c6b57dce9d8e530252ea9dbdc9d3a6649d6
tabegon <66781353+tabegon@users.noreply.github.com>
2025-04-01 20:14:51 +0200

    Update roi.py
    add echec sur arrivé

--- jeu_d_echec/jeu_d_echec/roi.py
+++ jeu_d_echec/jeu_d_echec/roi.py
@@ -1,3 +1,9 @@
+import pion as P
+import fou as B
+import cavalier as N
+import tour as R
+import dame as Q
+
 def identification(i, j):
     print('La pièce est un roi')
 
@@ -12,7 +18,9 @@ def move_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
     i_variation = i_origine - i_clic
     j_variation = j_origine - j_clic
     if abs(i_variation) <= 1 and abs(j_variation) <= 1:
-        return eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic)
+        mvmt_possible = eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic)
+    if mvmt_possible and echec_sur_arrive():
+        return True
         
 
 def eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
@@ -25,4 +33,28 @@ def eat_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
         elif piece == "w":
             if board[j_clic][i_clic] == piece_clic.upper():
                 return False
+    return True
+
+def echec_sur_arrive(board,player_turn,i_clic,j_clic):
+    nb_ligne = -1
+    nb_colonne = -1
+    piece = found_piece_color(board)
+    if piece == "b":
+        chaine = "RBNQKP"
+    elif piece == "w":
+        chaine = "rbnqkp"
+    for i in chaine.split():
+        for ligne in board:
+            nb_ligne += 1
+            nb_colonne = -1
+            for case in ligne.split():
+                nb_colonne += 1
+                if case == i:
+                    if piece == "b":
+                        if i.move_piece(board, player_turn, nb_ligne, nb_colonne, i_clic, j_clic):
+                            return False
+                    elif piece == "w":
+                        z = i.upper()
+                        if z.move_piece(board, player_turn, nb_ligne, nb_colonne, i_clic, j_clic):
+                            return False
     return True
