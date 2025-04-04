04064c77119703df71feac7affba8e4115cc07d6
tabegon <theo.abegon@gmail.com>
2025-04-04 10:21:10 +0200

    add echec

--- roi.py
+++ roi.py
@@ -127,3 +127,86 @@ def echec_sur_arrive(board,player_turn, i_origine,j_origine, i_clic,j_clic):
                             return False
     
     return True
+
+def echec(board,player_turn, i_origine,j_origine):
+    nb_ligne = -1
+    nb_colonne = -1
+    piece = found_piece_color(board, i_origine,j_origine)
+    if piece == "b":
+        chaine = "RBNQKP"
+    elif piece == "w":
+        chaine = "rbnqkp"
+    for i in chaine:
+        nb_ligne = -1
+        print(i)
+        nb_ligne = -1
+        for ligne in board:
+            nb_ligne += 1
+            nb_colonne = -1
+            for case in ligne:
+                nb_colonne += 1
+                if case == i:
+
+                    if i == 'R':
+                        if tour.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('la tour menace')
+                            return False
+                    
+                    if i == 'N':
+                        if cavalier.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('le cavaliver menace')
+                            return False
+                        
+                    if i == 'B':
+                        if fou.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('la tour menace')
+                            return False
+                    
+                    if i == 'Q':
+                        if dame.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('la reine menace a partir de', nb_ligne , nb_colonne, 'sur : ', i_clic,j_clic)
+                            print(i_clic, j_clic)
+                            return False
+                    
+                    if i == 'K':
+                        if mouvement_possible(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('le roi menace')
+                            return False
+                    
+                    if i == 'P':
+                        if pion.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('le pion menace')
+                            return False
+
+                    
+                    if i == 'r':
+                        if tour.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('la tour menace')
+                            return False
+                    
+                    if i == 'n':
+                        if cavalier.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('le cavalier menace')
+                            return False
+                        
+                    if i == 'b':
+                        if fou.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('le fou menace')
+                            return False
+                    
+                    if i == 'q':
+                        if dame.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('la dame menace')
+                            return False
+                    
+                    if i == 'k':
+                        if mouvement_possible(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('le roi menace')
+                            return False
+
+                    if i == 'p':
+                        if pion.move_piece(board, player_turn, nb_colonne, nb_ligne, i_origine, j_origine):
+                            print('le pion menace')
+                            return False
+    
+    return True
