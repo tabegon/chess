b7ec80296d7681e540388b84c2048c00c8db3113
AntoineBories <antoine.bories@gmail.com>
2025-04-08 22:32:22 +0200

    spécification + commentaire

--- roi.py
+++ roi.py
@@ -3,84 +3,120 @@ import fou
 import cavalier
 import tour
 import dame
-
+# on importe les move_piece de chaque piece pour l'échec, le pat et l'echec et mate
+# En esperant que les autres groupes aient fait du bon boulot
 def identification(i, j):
     print() 
     print('La pièce est un roi')
     print(i, j)
 
 def found_piece_color(board, i_origine, j_origine):
-    if board[j_origine][i_origine] == board[j_origine][i_origine].upper():
-        piece = "w"
+    """
+    trouve la couleur de la piece cliquée
+    @param i_origine, j_origine : coordonnée y, x de la pièce compris entre 0 et 7 inclu
+    @return piece : 'w' ou 'b'  
+    """
+    if board[j_origine][i_origine] == board[j_origine][i_origine].upper(): 
+        piece = "w"     # Si le caractére au coordoné du clic est une majuscule alors c'est une piéce blanche  
     else:
-        piece = "b"
+        piece = "b"     # Sinon c'est une piece noire 
     return piece
 
 def find_adverse_king(board, piece):
-    if piece == "b":
+    """
+    trouve les coordonées du roi adverse
+    @param board : plateau de jeu en tant que matrice
+    @param piece : variable contenant 'w' ou 'b'
+    @return i, j : coordonnés du roi adverse y, x
+    """
+    if piece == "b":                            # Si la piece est noir alors le roi adverse est le 'K' majuscule  
         roi_adverse = 'K'
-    elif piece == "w":
+    elif piece == "w":                          # Si la piece est blanche alors le roi adverse est le 'k' minuscule 
         roi_adverse = 'k'
     for j in range(8):
-        for i in range(8):
-            if board[j][i] == roi_adverse:
-                return i, j
+        for i in range(8):                      # On parcourt toute les cases du plateau 
+            if board[j][i] == roi_adverse:      # Si les coordonnées j, i sont égale au 'k' ou 'K' alors
+                return i, j                     # On retourne les coordonnées 
 
 def move_piece(board,player_turn,i_origine,j_origine,i_clic,j_clic):
-    global bouger
-    condition1 = mouvement_possible(board,player_turn,i_origine,j_origine,i_clic,j_clic)
+    """
+    décide si le roi est autorisé a bouger suivant tout un tas de condition 
+    @param board : plateau de jeu en tant que matrice 
+    @param player_turn : booléen blanc ou noir 
+    @param i_origine, j_origine : coordonnée y, x de la pièce sélectionnée compris entre 0 et 7 inclu
+    @param i_clic, j_clic : coordonnée y, x de la où on veut faire aller la piece compris entre 0 et 7 inclu
+    @return : booléen True/False qui conditionne la validation du coup 
+    """
+    global bouger                                                                           # utile pour le roque
+    condition1 = mouvement_possible(board,player_turn,i_origine,j_origine,i_clic,j_clic)    
     condition2 = echec_sur_arrive(board,player_turn, i_origine,j_origine, i_clic,j_clic)
     condition3 = eat_piece(board,i_origine,j_origine,i_clic,j_clic)
-    if condition1 and condition2 and condition3:
-        if turn(board, player_turn, i_origine, j_origine):
-            bouger = True
-            return True
-    return False
+    if condition1 and condition2 and condition3:               # Si les 3 conditions pour bouger sont True 
+        if turn(board, player_turn, i_origine, j_origine):     # Si c'est le tour du joueur 
+            bouger = True                                      # On met 'bouger' a True qui empeche le roque 
+            return True                                        # on retourne True qui fait bouger la piece
+    return False  # Si un des 3 trois conditions ne sont pas remplies ou que ce n'est pas le tour du joueur, on empeche le déplacement  
 
 def mouvement_possible(board,player_turn,i_origine,j_origine,i_clic,j_clic):
-    i_variation = i_origine - i_clic
+    """
+    détermine si le roi a le droit de faire ce déplacement en fonction de la variation de ces coordonnées 
+    @return : booléen 
+    """
+    i_variation = i_origine - i_clic        
     j_variation = j_origine - j_clic
-    if abs(i_variation) <= 1 and abs(j_variation) <= 1:
+    if abs(i_variation) <= 1 and abs(j_variation) <= 1: # Si le x et le y varie de 0 ou 1 alors on return 'True' 
         return True
     return False
 
 def eat_piece(board,i_origine,j_origine,i_clic,j_clic):
-    piece = found_piece_color(board, i_origine, j_origine)
+    """
+    détermine si on a le droit de manger la pièce suivant sa couleur 
+    @return : booléen 
+    """
+    piece = found_piece_color(board, i_origine, j_origine)  # On utilise ici la fonction found_piece_color pour determiner la couleur de la piece 
     piece_clic = board[j_clic][i_clic]
     if board[j_clic][i_clic] != ' ': 
         if piece == "b":
-            if board[j_clic][i_clic] == piece_clic.lower():
+            if board[j_clic][i_clic] == piece_clic.lower(): # Si la piece sur laquelle on clique est un piece noire alors False 
                 return False
         elif piece == "w":
-            if board[j_clic][i_clic] == piece_clic.upper():
+            if board[j_clic][i_clic] == piece_clic.upper(): # Si la piece sur laquelle on clique est un piece blanche alors False 
                 return False
-    return True
+    return True    # Sinon on return False 
 
 def turn(board, player_turn, i_origine, j_origine):
-    piece = found_piece_color(board, i_origine, j_origine)
-    if player_turn == piece:
+    """
+    determine si c'ets le tour du joueur 
+    @return : booléen 
+    """
+    piece = found_piece_color(board, i_origine, j_origine)  
+    if player_turn == piece:    #Si le tour est celui de la piece cliquée, on return True
         return True
 
 def echec_sur_arrive(board,player_turn, i_origine,j_origine, i_clic,j_clic):
+    """
+    détermine si la case sur laquelle le joueur a cliqué est menace par une pièce ou pas 
+    @return : booléen renseignant sur la possibilité pour le roi d'aller sur cette case 
+    """
     nb_ligne = -1
     nb_colonne = -1
     piece = found_piece_color(board, i_origine,j_origine)
     if piece == "b":
-        chaine = "RBNQKP"
+        chaine = "RBNQKP"   #Si la piece est noire alors on verifiera toutes les pieces blanches sous forme de string  
     elif piece == "w":
-        chaine = "rbnqkp"
-    for i in chaine:
-        nb_ligne = -1
+        chaine = "rbnqkp"   # Idem si la piece est blanche 
+    for i in chaine:        # Pour chaque piece dans le string 
+        nb_ligne = -1       
         for ligne in board:
-            nb_ligne += 1
-            nb_colonne = -1
-            for case in ligne:
-                nb_colonne += 1
+            nb_ligne += 1       # On ajoute 1 pour etre a la premiere ligne 
+            nb_colonne = -1     
+            for case in ligne: 
+                nb_colonne += 1 # On ajoute 1 pour etre a la premiere colonne 
                 if case == i:
 
-                    if i == 'R':
+                    if i == 'R':    # si le caractere de la case est 'R' 
                         if tour.move_piece(board, player_turn, nb_colonne, nb_ligne, i_clic, j_clic):
-                            return False
+                            return False  # Alors on regarde si la tour peut aller sur cette case et, si oui on retourne False 
                     
                     if i == 'N':
                         if cavalier.move_piece(board, player_turn, nb_colonne, nb_ligne, i_clic, j_clic):
@@ -104,7 +140,7 @@ def echec_sur_arrive(board,player_turn, i_origine,j_origine, i_clic,j_clic):
                             return False
 
                     
-                    if i == 'r':
+                    if i == 'r':    # Pareil mais pour les pieces noires 
                         if tour.move_piece(board, player_turn, nb_colonne, nb_ligne, i_clic, j_clic):
                             return False
                     
@@ -128,7 +164,7 @@ def echec_sur_arrive(board,player_turn, i_origine,j_origine, i_clic,j_clic):
                         if pion.move_piece(board, player_turn, nb_colonne, nb_ligne, i_clic, j_clic):
                             return False
     
-    return True
+    return True     # si aucune des pieces ne menace la case alors on return True 
 
 def echec(board,player_turn, i_origine,j_origine):
     return not echec_sur_arrive(board, player_turn, i_origine, j_origine, i_origine, j_origine) # Renvoie True quand le roi adverse est en echec
